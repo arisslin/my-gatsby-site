cecf2277272b55febbda65797e288e8e5db93ff6
Andreas Rissling <a.rissling@steinberg.de>
2023-11-23 17:25:09 +0100

    feat: Add a static image to the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import type { HeadFC, PageProps } from 'gatsby';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout pageTitle='Home Page'>
       <p>This is for testing</p>
+      <StaticImage
+        alt='Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera'
+        src='../images/clifford.jpeg'
+        width={300}
+      />
     </Layout>
   );
 };
